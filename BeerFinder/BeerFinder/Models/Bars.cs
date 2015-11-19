c85bacd5e06d26a18061d6321a27046a9244ac5f
frederic paquette <frederic_paquette@hotmail.com>
2015-11-19 10:06:30 -0500

    test

---
+++ BeerFinder/BeerFinder/Models/Bars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeerFinder.Models
+{
+    public class BarsRecord
+    {
+    }
+    public class BarsTable
+    {
+
+    }
+}
