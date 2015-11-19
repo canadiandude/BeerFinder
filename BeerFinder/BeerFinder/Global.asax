f828dc550ab8894482705f3fc6cde7985c9f34c1
canadiandude <francoisrioux67@hotmail.com>
2015-11-19 09:10:59 -0500

    Début du projet

---
+++ BeerFinder/BeerFinder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BeerFinder.MvcApplication" Language="C#" %>
---
+++ BeerFinder/BeerFinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeerFinder.Startup))]
+namespace BeerFinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
