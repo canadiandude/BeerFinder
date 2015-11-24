a2ecc9fc4d2417d80208be2ee22409b01304f584
frederic paquette <frederic_paquette@hotmail.com>
2015-11-24 09:12:08 -0500

    f

--- BeerFinder/BeerFinder/Models/Bars.cs
+++ BeerFinder/BeerFinder/Models/Bars.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,34 @@ namespace BeerFinder.Models
 {
     public class BarsRecord
     {
+        public long Id { get; set; }
+        [Display(Name = "Nom du Bar")]
+        [StringLength(50), Required]
+        public String Nom { get; set; }
+        [Display(Name = "Logo")]
+        [StringLength(50), Required]
+        public String Logo { get; set; }
+        [Display(Name = "Adresse")]
+        [StringLength(100), Required]
+        public String Adresse { get; set; }
+
     }
-    public class BarsTable
+    public class BarsTable:SqlExpressUtilities.SqlExpressWrapper
     {
+        public BarsRecord bar { get; set; }
+
+        public BarsTable(object conn)
+            : base(conn)
+        {
+            bar = new BarsRecord();
+            SetTableName("Bars");
+        }
+        public BarsTable()          
+        {
+            bar = new BarsRecord();
+            SetTableName("Bars");
+        }
+
 
     }
 }
---
+++ BeerFinder/BeerFinder/Models/Selections.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeerFinder.Models
+{
+    public class SelectionsRecord
+    {
+
+    }
+    public class SelectionTable
+    { 
+    
+    }
+}
